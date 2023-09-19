39ed4fa07b9aa5c25f43a71714cdc4151c3ba06f
Deaundra Davis <davis.deaundra@gmail.com>
2023-09-19 15:30:01 -0500

    Solution - Java Stdin/Stdout

---
+++ Guided Labs/src/guidedlabs/javaStdinStdout.java
@@ -0,0 +1,17 @@
+package guidedlabs;
+
+import java.util.Scanner;
+
+public class javaStdinStdout {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("42");
+        System.out.println("100");
+        System.out.println("125");    
+        
+        input.close();
+        
+    }
+}
