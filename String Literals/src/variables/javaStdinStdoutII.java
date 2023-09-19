06eec60e5dc969132b0e870c17fef4cf455b8ec3
Deaundra Davis <davis.deaundra@gmail.com>
2023-09-19 16:11:28 -0500

    javaStdinStdoutII

---
+++ String Literals/src/variables/javaStdinStdoutII.java
@@ -0,0 +1,19 @@
+package variables;
+
+import java.util.Scanner;
+
+public class javaStdinStdoutII {
+
+	public static void main(String[] args) {
+Scanner scan = new Scanner(System.in);
+        
+        int i = 42;
+        String s = "Welcome to HackerRank's Java tutorials!";
+        double d = 3.1415;
+    
+       
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
