3c3944038cc95cce3d488faa8733f6d694fe1bae
Deaundra Davis <davis.deaundra@gmail.com>
2023-09-19 09:59:46 -0500

    Third attempt

---
+++ Third Project 3/src/areaofcircle/AreaOfCircle3.java
@@ -0,0 +1,24 @@
+package areaofcircle;
+
+public class AreaOfCircle3 {
+
+	/**Main method */
+	
+	public static void main(String[] args) {
+		
+		double radius;
+		double area;
+		
+		// Assign a radius
+		radius = 20;
+		
+		// Compute area
+		area = radius * radius * 3.14159;
+		
+		// Display results
+		System.out.println("The area for the circle of radius " + 
+				radius + " is " + area);
+
+	}
+
+}
