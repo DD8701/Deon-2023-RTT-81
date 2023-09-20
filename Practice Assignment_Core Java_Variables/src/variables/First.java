257d8dcc12ad32b9fad256a8c9feb0bc699845fc
Deaundra Davis <davis.deaundra@gmail.com>
2023-09-19 23:03:05 -0500

    Question 1 Java Variables

---
+++ Practice Assignment_Core Java_Variables/src/variables/First.java
@@ -0,0 +1,20 @@
+package variables;
+
+public class First {
+
+	public static void main(String[] args) {
+		
+		// good coding practice is declaring all variables at the top
+		
+		// declares two integer variables and assigns an integer to each
+		int a = 10, b = 5, sum;
+		
+		// adds the integers together
+		sum = a + b;
+		
+		// assigns the sum to a variable
+	System.out.println(sum);
+
+	}
+
+}
