d9fcf9499fb74747b28a235871672124044255c6
Deaundra Davis <davis.deaundra@gmail.com>
2023-09-20 08:33:51 -0500

    Question 4 Java Variables

---
+++ Practice Assignment_Core Java_Variables/src/variables/Four.java
@@ -0,0 +1,23 @@
+package variables;
+
+public class Four {
+
+	public static void main(String[] args) {
+		
+		// declares 2 integers
+		int a;
+		double b;
+		
+		// assigns an integer to each
+		a = 23;
+		b = 54.5;
+		
+		// divides the larger number by the smaller number
+		double c;
+		c = b / a;
+		
+		
+		System.out.println(c);
+	}
+
+}
