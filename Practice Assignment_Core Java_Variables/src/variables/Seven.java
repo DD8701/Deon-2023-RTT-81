65f62278153814533370c5ae01ab0025bbcafbb0
Deaundra Davis <davis.deaundra@gmail.com>
2023-09-20 10:13:45 -0500

    Question 7 Java Variables

---
+++ Practice Assignment_Core Java_Variables/src/variables/Seven.java
@@ -0,0 +1,23 @@
+package variables;
+
+public class Seven {
+
+	public static void main(String[] args) {
+
+		// declares a named constant, final or static. final cannot change, static can be changed.
+		final double PI = 3.14;
+		
+		// uses named constant in a calculation
+		double a = 9;
+		double b = 5;
+		double x = a * b * PI;
+		
+		// prints the result
+		System.out.println(x);
+		
+		
+		
+
+	}
+
+}
