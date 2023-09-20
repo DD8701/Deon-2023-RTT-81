99ad37dc3d6a40570778da30c085ebea0b7b2032
Deaundra Davis <davis.deaundra@gmail.com>
2023-09-19 23:15:07 -0500

    Question 2 Java Variables

---
+++ Practice Assignment_Core Java_Variables/src/variables/Two.java
@@ -0,0 +1,23 @@
+package variables;
+
+public class Two {
+
+	public static void main(String[] args) {
+
+		// two double variables
+		double d;
+		double s;
+		
+		// assign a number to each variable
+		d = 1.2345;
+		s = 2.3456;
+		
+		// sum
+		double sum;
+		sum = d + s;
+		
+		System.out.println(sum);
+		
+	}
+
+}
