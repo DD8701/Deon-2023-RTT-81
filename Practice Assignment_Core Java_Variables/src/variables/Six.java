bb475caa84c614c5c959de888e7c6df0e2b82c24
Deaundra Davis <davis.deaundra@gmail.com>
2023-09-20 10:01:18 -0500

    Question 6 Java Variables

---
+++ Practice Assignment_Core Java_Variables/src/variables/Six.java
@@ -0,0 +1,29 @@
+package variables;
+
+public class Six {
+
+	public static void main(String[] args) {
+		
+		//  declares 2 integer variables and assign numbers
+		int x = 5;
+		int y = 6;
+		
+		// declare variable q and assign y/x to it and print q.
+		double q = y / x; // changed "int q" to "double q" before casting y to double and assigning it to q.
+		System.out.println(q);
+		
+		// cast y to a double and assign it to q. Print q again.
+		q = (double)y;
+		
+		// when you type cast double to int, put int in parenthesis next to the double variable and vice versa.
+		
+		
+	
+		
+		
+		
+
+
+	}
+
+}
