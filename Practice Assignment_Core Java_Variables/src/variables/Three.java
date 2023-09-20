17edc0e1233c1adeb5dfc45250b05b237c3f7171
Deaundra Davis <davis.deaundra@gmail.com>
2023-09-20 08:24:58 -0500

    Question 3 Java Variables

---
+++ Practice Assignment_Core Java_Variables/src/variables/Three.java
@@ -0,0 +1,23 @@
+package variables;
+
+public class Three {
+
+	public static void main(String[] args) {
+		
+		// declares an integer variable and a double variable
+		int i;
+		double d;
+		
+		// assigns numbers to each
+		i = 3;
+		d = 7.5489;
+		
+		// assigns the sum to variable and adds them together
+		double sum; 
+		sum = d + i;
+		
+		System.out.println(sum);
+
+	}
+
+}
