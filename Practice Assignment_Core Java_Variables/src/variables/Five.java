b404af4940fb993d1f68326a1f708bfe710af68f
Deaundra Davis <davis.deaundra@gmail.com>
2023-09-20 09:27:22 -0500

    Question 5 Java Variables

---
+++ Practice Assignment_Core Java_Variables/src/variables/Five.java
@@ -0,0 +1,27 @@
+package variables;
+
+public class Five {
+
+	public static void main(String[] args) {
+		
+		// declares 2 double variables
+		double x;
+		double y;
+		
+		// assigns a number to each
+		x = 5.23;
+		y = 6.87;
+		
+		// divides the larger number by the smaller number and assigns the result to a variable
+		double z  = y / x;
+		
+		// cast the result to an integer ???
+		System.out.println((int)z); // don't forget the extra set of parenthesis to complete the argument
+	
+	
+		
+
+	}
+
+
+}
