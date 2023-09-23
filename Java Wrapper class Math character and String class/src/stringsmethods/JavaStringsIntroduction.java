18b04054f854a60c981b3dd47abf166d0fa7c71b
Deaundra Davis <davis.deaundra@gmail.com>
2023-09-23 14:16:03 -0500

    Java String Introduction | HackerRank

---
+++ Java Wrapper class Math character and String class/src/stringsmethods/JavaStringsIntroduction.java
@@ -0,0 +1,21 @@
+package stringsmethods;
+
+public class JavaStringsIntroduction {
+
+	public static void main(String[] args) {
+		String str1 = "a";
+		String str2 = "b";
+		//
+		String sum = ("a" + "b");
+		System.out.println(str1.length() + str2.length());
+		if (str1.length() > str2.length()) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+		String str1upper = str1.toUpperCase();
+		String str2upper = str2.toUpperCase();
+		System.out.println(str1upper);
+		System.out.println(str2upper);
+	}
+}
