a32524c2d89b714bb3f9b57fb016c88beb3dd511
Deaundra Davis <davis.deaundra@gmail.com>
2023-09-25 09:43:05 -0500

    Loop demo 1

---
+++ Guided Labs/src/guidedlabs/LoopDemoOne.java
@@ -0,0 +1,19 @@
+package guidedlabs;
+
+public class LoopDemoOne {
+
+	public static void main(String[] args) {
+		// for loop syntax:
+		// for (initialization; termination;increment) {
+		// statement(s)
+		// }
+
+		// Program to print a text 5 times
+		int n = 5;
+		// for loop
+		for (int i = 1; i <= n; ++i) {
+			System.out.println("Java is fun");
+		}
+	}
+
+}
