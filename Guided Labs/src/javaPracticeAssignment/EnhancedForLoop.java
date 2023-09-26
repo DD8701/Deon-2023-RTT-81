fd66194ce159e36583ea5c61d26dd108c1ce85e8
Deaundra Davis <davis.deaundra@gmail.com>
2023-09-26 12:16:49 -0500

    GLabs

---
+++ Guided Labs/src/javaPracticeAssignment/EnhancedForLoop.java
@@ -0,0 +1,17 @@
+package javaPracticeAssignment;
+
+public class EnhancedForLoop {
+	
+    public static void main(String[] args) {
+    	
+    String names = "New York,Dallas,Las Vegas,Florida";
+    
+    String[] arrSplit = names.split(","); // Explain this please? arrSplit
+    
+    		for (int i = 0; i < arrSplit.length; i++) {
+    			
+    			System.out.println(arrSplit[i]);
+    			
+              }
+           }
+       }
