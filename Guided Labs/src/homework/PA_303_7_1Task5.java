93fa34d7f2af7e8ea8add53bb8b4d8c1ade279a3
Deaundra Davis <davis.deaundra@gmail.com>
2023-09-26 18:35:20 -0500

    task 5

---
+++ Guided Labs/src/homework/PA_303_7_1Task5.java
@@ -0,0 +1,24 @@
+package homework;
+
+public class PA_303_7_1Task5 {
+
+	public static void main(String[] args) {
+//------------------------------------------------------------------------------------------------------
+// 												Task 5:
+//-------------------------------------------------------------------------------------------------------
+// Write a program where you create an integer array with a length of 5. 
+// Loop through the array and assign the value of the loop control variable (e.g., i) 
+// to the corresponding index in the array.
+		
+		int[] arraySize = new int[5];
+		
+		for (int i = 0; i < 5; i++) {
+		System.out.println(arraySize[i]);
+		
+			
+		}
+		
+
+	}
+
+}
