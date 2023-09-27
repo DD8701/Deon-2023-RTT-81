64a65d532439675763506f840cbd2f3cc0404f77
Deaundra Davis <davis.deaundra@gmail.com>
2023-09-26 20:21:54 -0500

    task 6

---
+++ Guided Labs/src/homework/PA_303_7_1Task6.java
@@ -0,0 +1,31 @@
+package homework;
+
+public class PA_303_7_1Task6 {
+
+	public static void main(String[] args) {
+//----------------------------------------------------------------------------------------------------------
+// 												Task 6:
+//----------------------------------------------------------------------------------------------------------
+// Write a program where you create an integer array of 5 numbers
+		
+// Loop through the array and assign the value of the loop control variable
+		
+// multiplied by 2 to the corresponding index in the array.
+ 
+		int[] arraySize = new int[5]; // index is to 
+		for (int i = 0; i < 5; i++) {
+			//do stuff here.
+			 arraySize[i] = i*2;
+			 System.out.println(i*2);
+		}
+		
+		
+		
+		
+		
+		
+ 
+
+	}
+
+}
