2bf8b644f77f13b67b0530bacded498513d55342
Deaundra Davis <davis.deaundra@gmail.com>
2023-09-27 09:12:28 -0500

    task 7

---
+++ Guided Labs/src/homework/PA_303_7_1Task7.java
@@ -0,0 +1,24 @@
+package homework;
+
+public class PA_303_7_1Task7 {
+
+	public static void main(String[] args) {
+//------------------------------------------------------------------------------------------------
+// 												Task 7: 
+//---------------------------------------------------------------------------------------------------
+// Write a program where you create an array of 5 elements.
+		int[] element = {1,2,3,4,5};
+		
+// Loop through the array and print the value of each element,
+// except for the middle (index 2) element.
+		int high = element.length;
+		int mid = (high/2);
+		for (int i = 0; i < element.length; i++) {
+			//System.out.println(i);
+		}
+		
+
+
+	}
+
+}
