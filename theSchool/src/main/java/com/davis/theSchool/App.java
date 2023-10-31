3f613bb31600f8d603d55e50560031f55be0ac32
Deaundra Davis <davis.deaundra@gmail.com>
2023-10-30 19:57:30 -0500

    school

---
+++ theSchool/src/main/java/com/davis/theSchool/App.java
@@ -0,0 +1,18 @@
+package com.davis.theSchool;
+
+import com.davis.theSchool.util.GenerateDataUtil;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       /* GenerateDataUtil obj = new GenerateDataUtil();
+         obj.createDataManyToOne();*/
+    	
+       //GenerateDataUtil.createDataOneToMany();
+        //GenerateDataUtil.createDataOneToOne();
+        GenerateDataUtil.createDataManyToMany();
+    	
+    }
+}
